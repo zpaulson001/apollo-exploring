eda0c896bbab605fe853d538298b336e83c09c59
Zac <zpaulson001@gmail.com>
2024-11-29 18:17:59 -0800

    add resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import gql from 'graphql-tag';
+import { resolvers } from './resolvers.ts';
 
 const typeDefs = gql.gql(Deno.readTextFileSync('./schema.txt'));
 
 async function startApolloServer() {
-  const server = new ApolloServer({ typeDefs });
+  const server = new ApolloServer({ typeDefs, resolvers });
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
   });
