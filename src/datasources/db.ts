0db23f314ef67e91e2e27ea0161e0de34ba07c3c
Zac <zpaulson001@gmail.com>
2024-11-29 18:19:42 -0800

    change from dogs and users to trainers and pokemon

---
+++ src/datasources/db.ts
@@ -0,0 +1,16 @@
+let trainers = [
+  { id: 1, name: 'Ash', pokemon: [1, 2, 3] },
+  { id: 2, name: 'Misty', pokemon: [4, 5, 6] },
+  { id: 3, name: 'Brock', pokemon: [7, 8, 9] },
+  { id: 4, name: 'Gary', pokemon: [10, 11, 12] },
+  { id: 5, name: 'May', pokemon: [13, 14, 15] },
+  { id: 6, name: 'Dawn', pokemon: [16, 17, 18] },
+  { id: 7, name: 'Brendan', pokemon: [19, 20, 21] },
+  { id: 8, name: 'Maylene' },
+  { id: 9, name: 'Skyla', pokemon: [22, 23, 24] },
+  { id: 10, name: 'Bill', pokemon: [25, 26, 27] },
+];
+
+export default {
+  trainers,
+};
